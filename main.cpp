f19930fc352a79d6748d590bbbdd3d8f071629fc
Evan <epyama@gmail.com>
2017-10-02 15:38:32 -0400

    main.cpp from HW4

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  cout << "Hello World!" << endl; //Print hello world to the screen followed by an end line
+  return 0; //Exit the program
+}
